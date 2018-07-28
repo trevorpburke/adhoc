b73580cc3f5a0633e6a9f05da24f46e085b11196
tburke <tburke@switchfly.com>
2018-07-27 17:50:15 -0700

    add tablename arguments for models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 
 class Configuration(db.Model):
+    __tablename__ = 'configuration'
     id = db.Column(db.Integer, primary_key=True)
     config_name = db.Column(db.String(64), unique=True)
     username = db.Column(db.String(64))
@@ -8,10 +9,12 @@ class Configuration(db.Model):
     hostname = db.Column(db.String(64))
     database = db.Column(db.String(64))
     port = db.Column(db.String(10))
+    # TODO create DB relationships
 
 
-class Report(db.model):
+class Report(db.Model):
+    __tablename__ = 'report'
     id = db.Column(db.Integer, primary_key=True)
     report_name = db.Column(db.String(64), unique=True)
     query = db.Column(db.Text())
-
+    # TODO create DB relationships
