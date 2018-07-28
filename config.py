82d20175696677df924da8550f25c0f33bc76748
tburke <tburke@switchfly.com>
2018-07-27 17:31:26 -0700

    add sqlalchemy details to config class and add sqlalchemy db initiation to application instatiation module

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'asdf'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f'sqlite:///{os.path.join(basedir, "app.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
