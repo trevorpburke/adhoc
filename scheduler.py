a8e2d40ed911acccc60a25b277b2a19471099a11
tburke <tburke@switchfly.com>
2018-08-13 15:05:03 -0700

    change  function to add all new entries

--- scheduler.py
+++ scheduler.py
@@ -33,13 +33,11 @@ def find_new_reports():
     reports = Report.query.all()
     for r in reports:
         if r.report_name not in app.conf.beat_schedule:
-            app.conf.beat_schedule = {
-                r.report_name: {
-                    'task': 'scheduler.run_report',
-                    'schedule': crontab(hour=r.hour,
-                                        minute=r.minute,
-                                        day_of_week=r.day),
-                    'args': [r.id]
-                }
+            app.conf.beat_schedule[r.report_name] = {
+                'task': 'scheduler.run_report',
+                'schedule': crontab(hour=r.hour, minute=r.minute,
+                                    day_of_week=r.day),
+                'args': [r.id]
             }
+
     return app.conf.beat_schedule
