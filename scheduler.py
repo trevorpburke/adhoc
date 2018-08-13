f83dd082b3fca6ff503d58d3f82717f30c90765a
tburke <tburke@switchfly.com>
2018-08-13 14:59:08 -0700

    created startup checker function for celery

--- scheduler.py
+++ scheduler.py
@@ -9,6 +9,13 @@ from app.models import Report
 app = Celery('scheduler', broker='pyamqp://guest@localhost//')
 
 
+
+@app.on_after_configure.connect
+def check_new_reports(sender, **kwargs):
+    sender.add_periodic_task(30.0, find_new_reports(),
+                             name='find new reports every 30s')
+
+
 @app.task
 def run_report(report_id):
     """
@@ -25,12 +32,14 @@ def find_new_reports():
     """
     reports = Report.query.all()
     for r in reports:
-        app.conf.beat_schedule = {
-            r.report_name: {
-                'task': 'scheduler.run_report'
-                'schedule': crontab(hour=r.hour, minute=r.minute,
-                                    day_of_week=r.day),
-                'args': (r.id)
+        if r.report_name not in app.conf.beat_schedule:
+            app.conf.beat_schedule = {
+                r.report_name: {
+                    'task': 'scheduler.run_report',
+                    'schedule': crontab(hour=r.hour,
+                                        minute=r.minute,
+                                        day_of_week=r.day),
+                    'args': [r.id]
+                }
             }
-        }
     return app.conf.beat_schedule
